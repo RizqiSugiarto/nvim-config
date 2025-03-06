41117a7c15fe7c2083520f0ef1a04bd5cf285988
RizqiSugiarto <riskisugiarto290@gmail.com>
2025-03-06 22:24:08 +0700

    change: collor theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,9 +1,11 @@
-return  {
-    "ellisonleao/gruvbox.nvim", 
-    priority = 1000, 
-    config = function() 
-      vim.opt.background = "dark"
-      vim.cmd("colorscheme gruvbox")
-    end
-  }
-
+return {
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		vim.opt.background = "dark"
+		require("gruvbox").setup({
+			contrast = "hard",
+		})
+		vim.cmd("colorscheme gruvbox")
+	end,
+}
